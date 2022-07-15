98a374d2d873226f0714bc4345beced0d9b74c65
Víctor Álvarez <vicventum.job@gmail.com>
2022-07-15 14:27:18 -0400

    Obtiene el contenido del post en la página del detalle

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -17,12 +17,9 @@
 		</section>
 
 		<section class="post-content">
-			<h1 class="text-h3 mb-2">My first post!</h1>
-			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus amet et veritatis odit in minus dolorem sunt, quisquam quibusdam incidunt, aspernatur dolorum adipisci voluptates quaerat. Quas dolorum minima praesentium saepe porro atque nobis eius, est sequi illum laudantium perferendis ipsam omnis molestias consectetur ullam veritatis autem commodi doloribus officia. Tenetur culpa fugiat magnam natus. Consequuntur optio natus quis exercitationem iste vel veniam voluptatem ipsa. Accusantium, delectus. Consequuntur laudantium in possimus suscipit cupiditate repellendus voluptates, repudiandae aliquid, voluptatibus quisquam doloremque dolores quos distinctio iusto repellat fuga vitae, harum maiores aperiam veritatis sint reprehenderit unde! Possimus, quae. Sunt laboriosam adipisci veritatis reprehenderit!</p>
+			<h1 class="text-h3 mb-6">{{ post.title }}</h1>
 
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum tenetur architecto corporis repellendus. Natus provident aut doloribus reprehenderit sit ab exercitationem incidunt sequi error. Nesciunt maiores obcaecati ab tenetur ea inventore, eos vel itaque alias, accusamus quisquam quo vero sequi dolores consequuntur incidunt minima mollitia expedita quidem provident dolorem. Aperiam a magnam quos laboriosam nam aut nulla, deserunt sit iste ea pariatur minus odit voluptate. Dolorum odio in facere hic magni, non veniam distinctio. Quia vero rem, facilis eligendi illo expedita rerum provident magnam, doloremque cupiditate harum dolorem corrupti, obcaecati tenetur veniam explicabo tempore ratione cum ex deleniti sint. Deserunt dignissimos nulla qui iste expedita laborum reiciendis voluptatem, eveniet dolorum aliquid ipsam tempora praesentium atque, soluta incidunt. Assumenda, asperiores, tenetur unde culpa et exercitationem impedit aliquid ullam incidunt reiciendis aspernatur earum autem. Labore totam, maxime sapiente dolor pariatur similique quibusdam dicta repellendus ex quas eos id error, recusandae consectetur explicabo?</p>
-
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse ipsam, numquam rem tempore reprehenderit ex voluptate iure iusto, voluptatem nam, perferendis eveniet sapiente est aspernatur placeat. Quaerat odit itaque exercitationem!</p>
+			<nuxt-content :document="post" />
 		</section>
 
 		<The-Pagination class="d-flex justify-space-between mt-12" />
@@ -31,7 +28,13 @@
 
 <script>
 export default {
+	async asyncData({ $content, error, params }) {
+		const post = await $content(params.slug)
+			.fetch() // Obteniendo el post
+			.catch(() => error({ status: 404, message: 'Oops, looks like thah does not exist...' }))
 
+		return { post }
+	}
 }
 </script>
 
